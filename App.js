5e8501949e915c54e3e10b545f8865ae9943352f
anakokawa <133869844+anakokawa@users.noreply.github.com>
2024-04-16 12:28:14 +0000

    feat commit

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { Text, Button, View, StyleSheet, ImageBackground,ScrollView,Image } from
 
 import imgdavinci from './assets/imgdavinci.jpg';
 import imgmona from "./assets/mona.jpg";
+import imgvinci from "./assets/vinci.png";
 
 function Home({ navigation }) {
   return (
@@ -54,28 +55,30 @@ function Hitoria({ navigation }) {
   return (
 
     <View style={styles.container}>
-      <ImageBackground
-        source={imgdavinci}
-        style={{
-          flex: 1,
-          resizeMode: 'cover',
-          justifyContent: 'center',
-          width: "100%"
-        }}>
+      
         <View style={styles.container}>
           <View style={styles.content}>
 
-            <Text style={{ fontSize: 80, color: "#fff" }}>
-              NÃO
-            </Text>
             <ScrollView>
-              <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et consectetur erat. Phasellus consectetur nisl non magna aliquam, at feugiat turpis scelerisque. Sed quis tempor leo. Morbi quis iaculis lorem. Sed vel nisl eget lectus dapibus consectetur eget condimentum felis. Nam accumsan sollicitudin leo sed blandit. Quisque vitae libero nibh. Nam semper felis odio, nec elementum dui ultricies ac. Donec nec convallis nulla, id condimentum lorem. Nulla laoreet purus id porta maximus.
+            <Text style={{ fontSize: 40, color: "black" , alignItems:"center"}}>
+              QUEM FOI
+            </Text>
 
-Mauris sit amet fermentum lorem, vel imperdiet ligula. Fusce metus sem, facilisis in sem eget, egestas tristique enim. Suspendisse et rutrum erat. Phasellus id nunc semper, fringilla enim vitae, rutrum libero. Cras ac commodo arcu. Nulla vulputate lorem in porta tempor. Sed nec fringilla magna, et pellentesque lectus. Nunc faucibus metus turpis, vel viverra ligula dapibus id. Etiam gravida porta leo, vitae laoreet magna.
+              <Text>Leonardo Da Vinci foi um pintor e inventor nascido nos anos 1452.Em quanto os Eupeus estavam mais preocupados em colocarem seus pés na mais descoberta América
+                Leonardo Da Vinci queria saber como funcionavam os músculos do corpo humano, projetar Tanques de Guerra e cidades avançadas .
 
-Sed sodales enim eu facilisis faucibus. Duis facilisis nunc et nunc maximus rhoncus. Maecenas id vestibulum lectus, vitae aliquet lectus. Sed vel tellus tristique, congue ex eget, tristique turpis. Morbi aliquam vel urna tincidunt interdum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer lacinia mauris sit amet commodo dictum. Ut tempor lorem eu arcu mollis tristique. Aliquam imperdiet ex tortor, in auctor ante elementum quis. Integer laoreet purus eu consectetur mattis. Sed mattis est lorem, a euismod libero semper eu.
+              </Text>
+              <Image source={imgvinci} style={{borderRadius:50, width: "50%", height:"100%",justifyContent: 'center',}}/>
+              <Text></Text>
+            
+<Text style={{ fontSize: 40, color: "black" }}>
+              INVENÇÕES
+            </Text>
 
-Phasellus bibendum ex in hendrerit posuere. Donec a felis odio. Suspendisse ipsum magna, vehicula ac mollis et, vulputate id risus. Pellentesque condimentum euismod ornare. Vestibulum maximus, risus sit amet interdum fermentum, massa justo tristique ligula, a luctus tortor leo in nunc. Donec lacinia a enim vel rutrum. Vivamus nisl tellus, dignissim non quam lobortis, luctus finibus enim. Praesent ultricies lacinia feugiat. Praesent sit amet neque eu mauris venenatis egestas in a risus. Morbi luctus, elit at molestie condimentum, sapien orci bibendum augue, id malesuada ipsum nibh ac turpis. Mauris posuere justo et augue vestibulum, ac dictum ligula accumsan. Fusce et tempor metus.</Text>
+            <Text style={{ fontSize: 40, color: "black" }}>
+              QUADROS
+            </Text>
+            
             </ScrollView>
           </View>
           <View>
@@ -88,7 +91,7 @@ Phasellus bibendum ex in hendrerit posuere. Donec a felis odio. Suspendisse ipsu
 
           </View>
         </View>
-      </ImageBackground>
+      
     </View>
   )
 }
